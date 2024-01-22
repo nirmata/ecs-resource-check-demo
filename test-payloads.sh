85a58c3e26a85a5aac5cd50f8b693cf7beb06147
Boris Kurktchiev <boris.kurktchiev@nirmata.com>
2024-01-22 16:39:04 -0500

    add more report data
    Signed-off-by: Boris Kurktchiev <boris.kurktchiev@nirmata.com>

--- test-payloads.sh
+++ test-payloads.sh
@@ -3,6 +3,10 @@ echo "Checking Cluster Policies"
 echo "---------------------"
 kyverno-json scan --payload payloads/clusters.json --policy policies/cluster-fargate.yaml,policies/cluster-insights.yaml
 echo "\n"
+echo "Checking Service Policies"
+echo "---------------------"
+kyverno-json scan --payload payloads/create-ecs-service.json --policy policies/service-count.yaml
+echo "\n"
 echo "Checking Task Definition Policies"
 echo "---------------------"
 kyverno-json scan --payload payloads/definition.json --policy policies/definition-registry.yaml,policies/definition-rootfs.yaml
@@ -10,3 +14,7 @@ echo "\n"
 echo "Checking Task Policies"
 echo "---------------------"
 kyverno-json scan --payload payloads/task.json --policy policies/task-registry.yaml,policies/task-overrides.yaml
+echo "\n"
+echo "Checking CloudTrail Task Definition Registration"
+echo "---------------------"
+kyverno-json scan --payload payloads/register-task-definition.json --policy policies/register-task-definition-registry.yaml
