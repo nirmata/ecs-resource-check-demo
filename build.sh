b55b6be944ff96ad56330097658a2f14a2a77373
Boris Kurktchiev <boris.kurktchiev@nirmata.com>
2024-01-24 13:52:54 -0500

    protect the script run
    Signed-off-by: Boris Kurktchiev <boris.kurktchiev@nirmata.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if [[ -z ${1} ]]; then
+  echo Must pass in a tag version e.g.: ./build.sh v1
+  exit 1
+fi
+
 aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 844333597536.dkr.ecr.us-west-1.amazonaws.com
 
 docker build . -t ecs-lambda-function --no-cache=true --platform=linux/arm64 -f Dockerfile.kyverno-json
