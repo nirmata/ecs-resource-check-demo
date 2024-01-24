0f71b403e926cef6b183b743969c674e8049c7b0
Boris Kurktchiev <boris.kurktchiev@nirmata.com>
2024-01-24 13:46:44 -0500

    conform so i dont have to make new ECR repos
    Signed-off-by: Boris Kurktchiev <boris.kurktchiev@nirmata.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-docker build . -t ecs-lambda-function-kyverno --no-cache=true --platform=linux/arm64 -f Dockerfile.kyverno-json
-docker build . -t ecs-lambda-function-nctl --no-cache=true --platform=linux/arm64 -f Dockerfile.nctl
 aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 844333597536.dkr.ecr.us-west-1.amazonaws.com
-docker tag ecs-lambda-function-kyverno 844333597536.dkr.ecr.us-west-1.amazonaws.com/nirmata/ecs-lambda-function-kyverno:${1}
-docker push 844333597536.dkr.ecr.us-west-1.amazonaws.com/nirmata/ecs-lambda-function-kyverno:${1}
-docker tag ecs-lambda-function-nctl 844333597536.dkr.ecr.us-west-1.amazonaws.com/nirmata/ecs-lambda-function-nctl:${1}
-docker push 844333597536.dkr.ecr.us-west-1.amazonaws.com/nirmata/ecs-lambda-function-nctl:${1}
+
+docker build . -t ecs-lambda-function --no-cache=true --platform=linux/arm64 -f Dockerfile.kyverno-json
+docker tag ecs-lambda-function 844333597536.dkr.ecr.us-west-1.amazonaws.com/nirmata/ecs-lambda-function:${1}-kyverno
+docker push 844333597536.dkr.ecr.us-west-1.amazonaws.com/nirmata/ecs-lambda-function:${1}-kyverno
+
+docker build . -t ecs-lambda-function --no-cache=true --platform=linux/arm64 -f Dockerfile.nctl
+docker tag ecs-lambda-function 844333597536.dkr.ecr.us-west-1.amazonaws.com/nirmata/ecs-lambda-function:${1}-nctl
+docker push 844333597536.dkr.ecr.us-west-1.amazonaws.com/nirmata/ecs-lambda-function:${1}-nctl
