2c9cd21e646192f8ccf5cfda354b3e03bd512735
Boris Kurktchiev <boris.kurktchiev@nirmata.com>
2024-01-25 12:22:22 -0500

    move the old bits out of the way and under scripts
    Signed-off-by: Boris Kurktchiev <boris.kurktchiev@nirmata.com>

--- scripts/test-payloads.sh
+++ scripts/test-payloads.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 echo "Checking Cluster Policies"
 echo "---------------------"
-kyverno-json scan --payload payloads/clusters.json --policy policies/cluster-fargate.yaml,policies/cluster-insights.yaml
+kyverno-json scan --payload payloads/clusters.json --policy ../policies/cluster-fargate.yaml,../policies/cluster-insights.yaml
 echo "\n"
 echo "Checking Service Policies"
 echo "---------------------"
-kyverno-json scan --payload payloads/create-ecs-service.json --policy policies/service-count.yaml
+kyverno-json scan --payload payloads/create-ecs-service.json --policy ../policies/service-count.yaml
 echo "\n"
 echo "Checking Task Definition Policies"
 echo "---------------------"
-kyverno-json scan --payload payloads/definition.json --policy policies/definition-registry.yaml,policies/definition-rootfs.yaml
+kyverno-json scan --payload payloads/definition.json --policy ../policies/definition-registry.yaml,../policies/definition-rootfs.yaml
 echo "\n"
 echo "Checking Task Policies"
 echo "---------------------"
-kyverno-json scan --payload payloads/task.json --policy policies/task-registry.yaml,policies/task-overrides.yaml
+kyverno-json scan --payload payloads/task.json --policy ../policies/task-registry.yaml,../policies/task-overrides.yaml
 echo "\n"
 echo "Checking CloudTrail Task Definition Registration"
 echo "---------------------"
-kyverno-json scan --payload payloads/register-task-definition.json --policy policies/register-task-definition-registry.yaml
+kyverno-json scan --payload payloads/register-task-definition.json --policy ../policies/register-task-definition-registry.yaml
