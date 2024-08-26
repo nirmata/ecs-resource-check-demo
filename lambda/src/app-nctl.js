1061d0beb8dbe2973ff52298fe74e1bae601f702
Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>
2024-08-26 13:34:06 -0400

    got working code
    Signed-off-by: Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>

--- lambda/src/app-nctl.js
+++ lambda/src/app-nctl.js
@@ -26,47 +26,57 @@ exports.handler = async (event, context) => {
       fs.mkdirSync(resourceDir);
     }
 
+    // console.log(`Returned Event: ${resourceYAML}`)
     fs.writeFileSync("/tmp/resources/resource.yaml", resourceYAML, "utf8");
 
     // Run a CLI command to verify the task
-    const command = `/bin/nctl scan json -p /policies/ -r /tmp/resources/resource.yaml --details --publish --token ${token}`;
-    const results = execSync(command);
-    console.log(`Got results: ${results.toString()}`);
+    try {
+      const command = `/bin/nctl scan json -p /policies/ -r /tmp/resources/resource.yaml -o json --publish --token ${token}`;
+      const results = execSync(command);
 
-    var eventName = "--";
-    if (event.detail.eventName) {
-      eventName = event.detail.eventName;
+      if (results) {
+        console.log(`Got nctl results: ${results.toString()}`);
+      } else {
+        console.log(`Results are empty`);
+      }
+    } catch (err) {
+      console.log(`Got nctl error: ${err}`);
     }
-    var eventTime = event.time;
-    if (event.detail.eventTime) {
-      eventTime = event.detail.eventTime;
-    }
-    var resources = [];
-    resources.push("Event Source: " + event.source);
-    resources.push("Event Name: " + eventName);
-    resources.push("Event Type: " + event["detail-type"]);
-    resources.push("Event Time: " + eventTime);
-    resources.push("AWS Account: " + event.account);
-    resources.push("Region: " + event.region);
-    for (const resource of event.resources) {
-      resources.push("Resource: " + resource);
-    }
-    console.log(`Got resources: ${resources}`);
-    const response = await client.send(
-      new PutEventsCommand({
-        Entries: [
-          {
-            Detail: JSON.stringify(details),
-            DetailType: "Policy Results",
-            Resources: event.resources,
-            Source: "nirmata.lambda",
-            EventBusName:
-              "arn:aws:events:us-west-1:844333597536:event-bus/ecs-results-event-bus",
-          },
-        ],
-      })
-    );
-    console.log(`PutEvents response: ${JSON.stringify(response)}`);
+
+    // var eventName = "--";
+    // if (event.detail.eventName) {
+    //   eventName = event.detail.eventName;
+    // }
+    // var eventTime = event.time;
+    // if (event.detail.eventTime) {
+    //   eventTime = event.detail.eventTime;
+    // }
+    // var resources = [];
+    // resources.push("Event Source: " + event.source);
+    // resources.push("Event Name: " + eventName);
+    // resources.push("Event Type: " + event["detail-type"]);
+    // resources.push("Event Time: " + eventTime);
+    // resources.push("AWS Account: " + event.account);
+    // resources.push("Region: " + event.region);
+    // for (const resource of event.resources) {
+    //   resources.push("Resource: " + resource);
+    // }
+    // console.log(`Got resources: ${resources}`);
+    // const response = await client.send(
+    //   new PutEventsCommand({
+    //     Entries: [
+    //       {
+    //         Detail: JSON.stringify(details),
+    //         DetailType: "Policy Results",
+    //         Resources: event.resources,
+    //         Source: "nirmata.lambda",
+    //         EventBusName:
+    //           "arn:aws:events:us-west-1:844333597536:event-bus/ecs-results-event-bus",
+    //       },
+    //     ],
+    //   })
+    // );
+    // console.log(`PutEvents response: ${JSON.stringify(response)}`);
     return;
   } catch (err) {
     console.error(err);
