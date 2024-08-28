e4507a35ba17af1e5497483dfdba5370283fa578
Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>
2024-08-28 17:59:39 -0400

    remove supurflus files
    Signed-off-by: Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>

--- lambda/build.sh
+++ lambda/build.sh
@@ -8,6 +8,6 @@ fi
 # This is specific to Nirmata's AWS account
 aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 844333597536.dkr.ecr.us-west-1.amazonaws.com
 
-docker buildx build . -t ecs-lambda-function --no-cache=true --provenance=false -f Dockerfile.nctl
+docker buildx build . -t ecs-lambda-function --no-cache=true --provenance=false -f Dockerfile
 docker tag ecs-lambda-function 844333597536.dkr.ecr.us-west-1.amazonaws.com/nirmata/ecs-lambda-function:${1}-nctl
 docker push 844333597536.dkr.ecr.us-west-1.amazonaws.com/nirmata/ecs-lambda-function:${1}-nctl
